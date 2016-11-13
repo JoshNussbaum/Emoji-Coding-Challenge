cb00cd2893b73a2dbbec30d956a48ec557f44c3b
Josh Nussbaum <joshn@surveymonkey.com>
2016-11-13 15:31:23 -0800

    Initial Commit

---
+++ CodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CodingChallenge
+//
+//  Created by Josh Nussbaum on 11/13/16.
+//  Copyright © 2016 Josh Nussbaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CodingChallenge
+//
+//  Created by Josh Nussbaum on 11/13/16.
+//  Copyright © 2016 Josh Nussbaum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
